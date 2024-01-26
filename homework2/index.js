b235bae6f0c2bd7c8e47800c95cb247a41f53146
shunoks <oksana.shundryna@gmail.com>
2024-01-26 22:38:43 +0200

    lection2 pls work

--- homework2/index.js
+++ homework2/index.js
@@ -2,4 +2,5 @@ import chalk from "chalk";
 const hello = "Hello";
 let variable = 12;
 
-console.log(chalk.red(hello), chalk.blue(variable), chalk.yellow("people"))
+console.log(chalk.red(hello), chalk.blue(variable), chalk.yellow("people"));
+console.log("Please work!!!");
