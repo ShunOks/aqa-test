a7a0332d85ecb8eae7128721ff0966aa322c6bb9
shunoks <oksana.shundryna@gmail.com>
2024-01-26 22:29:38 +0200

    lection2 fix bugs

--- homework2/index.js
+++ homework2/index.js
@@ -2,4 +2,4 @@ import chalk from "chalk";
 const hello = "Hello";
 let variable = 12;
 
-console.log(chalk.red(hello), chalk.blue(variable), yellow("people"))
+console.log(chalk.red(hello), chalk.blue(variable), chalk.yellow("people"))
