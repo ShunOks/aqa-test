f417dea0116125dc2b04c5677dee1edba3083cd5
ShunOks <157057212+ShunOks@users.noreply.github.com>
2024-02-03 15:18:14 +0200

    Lection5 (#5)
    * lection4
    
    * homework5

---
+++ homework5/task1.js
@@ -0,0 +1,22 @@
+//function declaratoion
+function square1 (width, height) {
+    return width*height;
+}
+
+// function expression
+const square2 = function (width, height) {
+    return width*height;
+}
+
+// arrow function
+const square3 = (width, height) =>  {
+    return width*height;
+}
+
+// concise arrow function
+const square4 = (width, height) => width*height;
+
+console. log('function declaratoion realisation: ', square1(5,10));
+console. log('function expression: ', square2(2,5));
+console. log('arrow function: ', square3(3,6));
+console. log('concise arrow function: ', square4(4,8));
